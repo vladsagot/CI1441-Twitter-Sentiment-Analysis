1370829f3fc3a1ddab8f11ff2dac745f60bb945e
vladimir <vladimir.sagot@ucr.ac.cr>
2019-11-23 13:23:59 -0600

    Init

--- tweets.py
+++ tweets.py
@@ -1,11 +1,60 @@
 import pandas as pd
+import numpy as np
+import re
+import nltk
+import tensorflow as tf
 import seaborn as sns
+from nltk.corpus import stopwords
+
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten
+from keras.layers import GlobalMaxPooling1D
+from keras.layers.embeddings import Embedding
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+
 import matplotlib.pyplot as plt
+
+
+def preprocess_text(sen):
+    # Removing html tags
+    sentence = remove_tags(sen)
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    #Removing
+
+    return sentence
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
 
 tweet_reviews = pd.read_csv("/home/vladimir/Desktop/dataset.csv")
 
-print(tweet_reviews)
+# print(tweet_reviews)
 
-sns.countplot(x='class', data=tweet_reviews)
+# sns.countplot(x='class', data=tweet_reviews)
 
-plt.show()
+# plt.show()
+
+var = preprocess_text(tweet_reviews["tweet"][1])
+
+print(tweet_reviews["tweet"][1])
+print(var)
+
